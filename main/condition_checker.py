c689f38a26bf6e19accbe2db1b765809f9c5236a
chompa111 <gustavo.lach@gmail.com>
2019-10-04 23:13:46 -0300

    protótipo com string

--- main/condition_checker.py
+++ main/condition_checker.py
@@ -10,16 +10,48 @@ class Checker:
             action_preconditions = a[1]
             action_args = a[2]
             action_effects = a[3]
+
+            self.testaPrecond(("banana","cafe","rainer"),action_args,action_preconditions,"")
             print(action_name)
             print('--- args:')
             print([(t.name, t.type) for t in action_args])
             print('--- preconditions:')
             for prec in action_preconditions:
-                print(re.split('[(,)]', str(prec))[:-1])
+                print(re.split('YYY', str(prec)))
             print('--- effects:')
-            print(action_effects)
+            print(str(action_effects[0]))
             print()
         pass
+
+    def testaPrecond(self,tuplaArg,args,preconds,state):
+
+        pares={}
+        for i,j in zip(tuplaArg,args):
+            pares[j.name]=i
+
+        for precond in preconds:
+            predicado=str(precond)
+            for argPrecond in precond.predicate.args:
+                predicado=predicado.replace(argPrecond,pares[argPrecond])
+            pass
+            print("PREDICADO:",predicado)
+
+        #TODO DEVOLVE VERDADEIRO SE O ESTADO CONTEM TODOS
+
+    def getEfects(self, tuplaArg, args, preconds, state):
+
+        pares = {}
+        for i, j in zip (tuplaArg, args):
+            pares[j.name] = i
+
+        for precond in preconds:
+            predicado = str (precond)
+            for argPrecond in precond.predicate.args:
+                predicado = predicado.replace (argPrecond, pares[argPrecond])
+            pass
+            print ("PREDICADO:", predicado)
+
+        # TODO DEVOLVE VERDADEIRO SE O ESTADO CONTEM TODOS
 
     def next_states(self):
         pass
