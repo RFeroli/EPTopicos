94ce5daf5fccf235fddbf88bcfcbd2ee8f86adc2
Rainer Oliveira <rainer.henrique.oliveira@usp.br>
2019-10-09 00:24:10 -0300

    Progresso no conversor

--- in/tyreworld_domain.pddl
+++ in/tyreworld_domain.pddl
@@ -1,10 +1,9 @@
-;; PDDL Domain Specification  File
-
 (define (domain tyreworld)
 	(:requirements :strips :equality :typing)
     (:types obj - object
 	  tool wheel nut - obj
 	  container hub - object)
+	(:constants wrench jack pump - tool)
     (:predicates (open ?x)
              (closed ?x)
              (have ?x)
--- main/conversorPDDL.py
+++ main/conversorPDDL.py
@@ -1,23 +1,46 @@
 import re
+import pyparsing
 
 class Conversor:
+
     def __init__(self, domain, problem):
+        self.domain = {}
+        self.problem = {}
         self.domain_file = domain
         self.problem_file = problem
         with open(self.domain_file, 'r') as f:
             self.parse_domain(f)
+        with open(self.problem_file, 'r') as f:
+            self.parse_problem(f)
 
     def parse_domain(self, f):
-        for line in f.readlines():
-            line = line.strip()
-            if re.match(r';.*', line) or line == '':
-                continue
-            print(line)
+        domain = ' '.join([line.strip() for line in f.readlines() if not re.match(r';.*', line) and line != '\n'])
+        args = self.get(domain)
+        print(args)
+
+    def get(self, s, level=0, i=0):
+        tree = {}
+        token = []
+        opener = {}
+        while i < len(s):
+            if s[i] == '(':
+                opener[level] = i
+                level += 1
+            elif s[i] == ')':
+                level -= 1
+                tree[level] = tree.get(level, [])
+                tree[level].append(s[opener[level]+1:i])
+            i += 1
+        return tree[1]
+
+
     def parse_problem(self, f):
-        pass
+        problem = ' '.join([line.strip() for line in f.readlines() if not re.match(r';.*', line) and line != '\n'])
+        args = self.get(problem)
+        print(args)
 
 
 path = '../in/'
 
-Conversor(path + 'robot_domain.pddl','robot_problem.pddl')
-Conversor(path + 'tyreworld_domain.pddl','tyreworld_problem.pddl')
+Conversor(path + 'robot_domain.pddl', path + 'robot_problem.pddl')
+Conversor(path + 'tyreworld_domain.pddl', path + 'tyreworld_problem.pddl')
